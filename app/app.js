fd9119da48ebb7ba099fcb5aace8ef0b079e3eeb
achrafbenyounes <Achraf@192.168.1.2>
2014-09-08 01:02:58 +0200

    Define new controller in app.js
    Define a flag called "show" within the controller to work with ng-show angular directive
    
    Try the "uppercase|lowercase filter" and applying them on textfields
    
    Try the ng-repeat directive to loop through a collection of values

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
@@ -10,3 +10,13 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+app.controller("WelcomeUsersControler", function() {
+   alert("Welcome Mr Achraf Ben Younes, you do an awsome job for learning "
+   + "Angular JS"); 
+   
+   var listUsers = ["Stephane kendergi", "Achraf BEN YOUNES", "Oussama Ben younes"];
+   this.users = listUsers;
+   this.show = true;
+});
+
